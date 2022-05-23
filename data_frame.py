e39c8634becfe67ac70b6b9f70a27a3022677055
CrucianFrik <kokhanova.va@phystech.edu>
2022-05-23 04:38:43 +0300

    nthg

--- data_frame.py
+++ data_frame.py
@@ -28,11 +28,30 @@ class DataFrame:
 
     def request(self, msg):
         self.__check_update()
-        return self.__parser_local.parse(msg)
+        return self.__to_json(self.__parser_local.parse(msg))
 
     def print_tables(self):  # to del
         for i in self.__tables:
             print(i.get___data())
+
+    def __to_json(self, df):
+        ans = {}
+        for num, line in df.iterrows():
+            weekday = self.get_table("weekdays").get_data().loc[line[0]].weekday
+            time = self.get_table("times").get_data().loc[line[1]].time
+            group = self.get_table("groups").get_data().loc[line[2]].group
+            class_ = self.get_table("classes").get_data().loc[line[3]].number
+            teacher = self.get_table("teachers").get_data().loc[line[4]]
+            teacher = ' '.join([teacher.surname, teacher["name"], teacher.lastname])
+            subject = line[5]
+            ans[weekday] = ans.get(weekday, {})
+            ans[weekday][time] = ans[weekday].get(time, [group, class_, teacher, subject])
+            print(weekday, time, group, class_, teacher, subject)
+        print(ans)
+        return ans
+
+
+
 
     def __update(self):
         self.__tables = self.__parser_global.parse()
